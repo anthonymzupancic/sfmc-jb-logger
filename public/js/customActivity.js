24154c63390221bda89bcce90101b1115f03c21e
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-07-12 21:49:51 -0500

    update

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -79,7 +79,7 @@ define([
                         html += '<input class="w-full-textarea" name="exitCode" id="exitCode" />';
                         html += '</div>';
 
-                        $('#loggingFields').innerHTML(html)
+                        $('#loggingFields').append(html)
                     } else {
                         let html = '';
 
@@ -97,7 +97,7 @@ define([
                             html += '</div>'
                         })
 
-                        $('#loggingFields').innerHTML(html)
+                        $('#loggingFields').append(html)
 
                     }
                 });
