bb532ddfb9e62b0d1ff7a02bb16270ee5486d722
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-07-19 16:15:49 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,26 @@ var activity = require('./activity');
 /*
  * GET home page.
  */
-exports.index = function(req, res){
-    if( !req.session.token ) {
-        res.render( 'index', {
+exports.index = function(req, res) {
+    console.log(req)
+    if (!req.session.token) {
+        res.render('index', {
             title: 'Unauthenticated',
             errorMessage: 'This app may only be loaded via Salesforce Marketing Cloud',
         });
     } else {
-        res.render( 'index', {
+        res.render('index', {
             title: 'Journey Builder Activity',
             results: activity.logExecuteData,
         });
     }
 };
 
-exports.login = function( req, res ) {
-    console.log( 'req.body: ', req.body );
-    res.redirect( '/' );
+exports.login = function(req, res) {
+    console.log('req.body: ', req.body);
+    res.redirect('/');
 };
 
-exports.logout = function( req, res ) {
+exports.logout = function(req, res) {
     req.session.token = '';
 };
