23d4f9f731338a2943103df434d06bc0eeb1d56e
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-07-21 09:17:50 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,7 @@ exports.authorize = function(req, res) {
     const authBase = 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id='
     let redirectURI = 'https%3A%2F%2Ftwilio-integration-dev.herokuapp.com%2F';
 
-    res.redirect(`${authBase}${process.env.sfmcAuthClientID}&redirect_uri=${redirectURI}`)
+    res.redirect('/authenticate')
 }
 
 exports.authenticate = function(req, res) {
