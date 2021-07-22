16c6b28beebf267a04826d4249c11f4450bc7cf3
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-07-22 08:05:37 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,7 @@ exports.index = async function(req, res) {
             let validation = await validateAuthCode(config, code)
             console.log(validation)
             if (validation.data.access_token) {
-                res.send('valid')
+                res.sendFile('./public/index.html')
             } else {
                 res.send('Unauthorized')
             }
