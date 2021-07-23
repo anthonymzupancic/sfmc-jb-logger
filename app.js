a5d9425294b8abf875d0f96f29e2b1f1f980ff03
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-07-23 07:39:30 -0500

    update

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var routes = require('./routes');
 var activity = require('./routes/activity');
 const JWT = require(path.join(__dirname, 'lib', 'jwtDecoder.js'));
 const axios = require('axios');
+const { query } = require('express');
 
 var app = express();
 
@@ -24,23 +25,57 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.methodOverride());
 //app.use(express.favicon());
 
+async function validateAuthCode(config, code) {
+    try {
 
-// // Simple custom middleware
-// function tokenFromJWT(err, req, res, next) {
-//     if (err) {
-//         res.status(404).send('Unauthorized')
-//     }
-//     // Setup the signature for decoding the JWT
-//     var jwt = new JWT({ appSignature: process.env.jwtSecret });
+        let authCheck = await axios.post(config.url, config.options)
+        return authCheck
 
-//     // Object representing the data in the JWT
-//     var jwtData = jwt.decode(req);
+    } catch (err) {
 
-//     // Bolt the data we need to make this call onto the session.
-//     req.session.token = jwtData.token;
-//     next();
-// }
+        return err
 
+    }
+}
+
+const getCode = ((req, res, next) => {
+    if (req.query.code) {
+        next()
+    } else {
+        const authBase = 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id='
+        res.redirect(`${authBase}${process.env.sfmcAuthClientID}&redirect_uri=${redirectURI}`)
+    }
+})
+
+const verifyCode = ((req, res, next) => {
+    try {
+        if (req.query.code) {
+            const config = {
+                url: 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/token',
+                options: {
+                    "grant_type": "authorization_code",
+                    "code": req.query.code,
+                    "client_id": process.env.sfmcAuthClientID,
+                    "client_secret": process.env.sfmcAuthClientSecret,
+                    "redirect_uri": "https://twilio-integration-dev.herokuapp.com/authorize"
+                }
+            }
+
+            let validation = await validateAuthCode(config, code)
+            console.log(validation)
+            if (!validation.data.access_token) {
+                res.send('Unauthorized')
+            } else {
+                next()
+            }
+        } else {
+
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+})
 
 
 
@@ -48,6 +83,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if ('development' == app.get('env')) {
     app.use(errorhandler());
 }
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HubExchange Routes
 app.use('/', routes.index);
