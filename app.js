e1087a2d745eef9b1dcabab68b195974c3a3ebaf
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-07-28 09:16:08 -0500

    update

--- app.js
+++ app.js
@@ -27,79 +27,70 @@ app.use(cookieParser('testSecret-jb-logger'));
 //app.use(express.methodOverride());
 //app.use(express.favicon());
 app.use((req, res, next) => {
-    console.log(req.signedCookies)
+    console.log(req)
     next()
 })
 
-app.use((req, res, next) => {
-    if (!req.sessionID) {
-        if (req.query.code) {
-            next()
-        } else {
-            let redirectURI = 'https%3A%2F%2Ftwilio-integration-dev.herokuapp.com';
-            const authBase = 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id='
-            res.redirect(`${authBase}${process.env.sfmcAuthClientID}&redirect_uri=${redirectURI}`)
-        }
-    } else {
-        next()
-    }
+// app.use((req, res, next) => {
+//         if (req.query.code) {
+//             next()
+//         } else {
+//             let redirectURI = 'https%3A%2F%2Ftwilio-integration-dev.herokuapp.com';
+//             const authBase = 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id='
+//             res.redirect(`${authBase}${process.env.sfmcAuthClientID}&redirect_uri=${redirectURI}`)
+//         }
+// })
 
-})
+// app.use((req, res, next) => {
+//         try {
+//             const code = req.query.code
 
-app.use((req, res, next) => {
-    if (!req.sessionID) {
-        try {
-            const code = req.query.code
+//             if (code) {
+//                 const config = {
+//                     url: 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/token',
+//                     options: {
+//                         "grant_type": "authorization_code",
+//                         "code": code,
+//                         "client_id": process.env.sfmcAuthClientID,
+//                         "client_secret": process.env.sfmcAuthClientSecret,
+//                         "redirect_uri": "https://twilio-integration-dev.herokuapp.com"
+//                     }
+//                 }
 
-            if (code) {
-                const config = {
-                    url: 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/token',
-                    options: {
-                        "grant_type": "authorization_code",
-                        "code": code,
-                        "client_id": process.env.sfmcAuthClientID,
-                        "client_secret": process.env.sfmcAuthClientSecret,
-                        "redirect_uri": "https://twilio-integration-dev.herokuapp.com"
-                    }
-                }
+//                 axios.post(config.url, config.options)
+//                     .then((res) => {
+//                         if (!res.data.access_token) {
+//                             res.send('Unauthorized')
+//                         } else {
+//                             console.log('Access Token Found')
+//                             console.log(res.data.access_token)
+//                             next()
+//                         }
+//                     })
+//                     .catch((err) => {
+//                         console.log(err)
+//                         res.send(err)
+//                     })
 
-                axios.post(config.url, config.options)
-                    .then((res) => {
-                        if (!res.data.access_token) {
-                            res.send('Unauthorized')
-                        } else {
-                            console.log('Access Token Found')
-                            console.log(res.data.access_token)
-                            next()
-                        }
-                    })
-                    .catch((err) => {
-                        console.log(err)
-                        res.send(err)
-                    })
-
-            } else {
-                res.send('Unautorized: no code provided.')
-            }
-        } catch (err) {
-            console.log(err)
-        }
-    } else {
-        next()
-    }
-})
+//             } else {
+//                 res.send('Unautorized: no code provided.')
+//             }
+//         } catch (err) {
+//             console.log(err)
+//         }
+// })
 
 
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-    name: 'sfmc-jb-logger',
-    secret: 'testSecret-jb-logger',
-    maxAge: 6000 * 1,
-    resave: false,
-    saveUninitialized: false,
-    sameSite: 'strict',
-    cookie: { secure: false }
-}))
+// app.set('trust proxy', 1) // trust first proxy
+// app.use(session({
+//     name: 'sfmc-jb-logger',
+//     secret: 'testSecret-jb-logger',
+//     maxAge: 6000 * 1,
+//     resave: false,
+//     saveUninitialized: false,
+//     sameSite: 'strict',
+//     cookie: { secure: false }
+// }))
 
 
 app.use(express.static(path.join(__dirname, 'public')))
