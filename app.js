687bc52f37d03be7ad3a53dc8a862a951bc1f6ba
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-07-29 13:05:49 -0500

    update index route

--- app.js
+++ app.js
@@ -89,14 +89,9 @@ app.use(cookieParser('testSecret-jb-logger'));
 //     cookie: { secure: false }
 // }))
 
-//app.use('/', routes.authorize);
-
-app.use((req, res, next) => {
-    console.log('**** Next Route Used *** ')
-    next()
-})
-
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', routes.authorize);
+
 
 // Express in Development Mode
 if ('development' == app.get('env')) {
