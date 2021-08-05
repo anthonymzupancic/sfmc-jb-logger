a2320988cbd5b81191ba803fe87df249b9ed14d6
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-08-05 12:43:25 -0500

    update

--- app.js
+++ app.js
@@ -108,6 +108,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // //sleep
 app.use(function(req, res, next) {
+    console.log('wait')
     await delay(1000) /// waiting 1 second.
 
     next();
