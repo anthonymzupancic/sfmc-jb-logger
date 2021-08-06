ef34a16fa5cbb2a96d3a46f8b1b7a12ff347aa68
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-08-06 15:54:58 -0500

    update

--- routes/middleware.js
+++ routes/middleware.js
@@ -31,58 +31,58 @@ exports.cookie = function(req, res, next) {
 exports.authorize = function(req, res, next) {
     console.log('*** Authorize Endpoint ***')
     console.log('*** session ***')
-    console.log(req.session)
+    console.log(req.jbLoggerSession)
 
-    if (typeof req.session === 'undefined' || typeof req.session.auth === 'undefined' || !req.session.auth) {
-        if (!req.query.code) {
-            console.log('*** Retrieve Code ***')
+    // if (typeof req.session === 'undefined' || typeof req.session.auth === 'undefined' || !req.session.auth) {
+    if (!req.query.code) {
+        console.log('*** Retrieve Code ***')
 
-            let redirectURI = 'https%3A%2F%2Ftwilio-integration-dev.herokuapp.com';
-            const authBase = 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id='
-            res.redirect(`${authBase}${process.env.sfmcAuthClientID}&redirect_uri=${redirectURI}`)
-        } else if (req.query.code) {
-            console.log('*** Validate Code ***')
+        let redirectURI = 'https%3A%2F%2Ftwilio-integration-dev.herokuapp.com';
+        const authBase = 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id='
+        res.redirect(`${authBase}${process.env.sfmcAuthClientID}&redirect_uri=${redirectURI}`)
+    } else if (req.query.code) {
+        console.log('*** Validate Code ***')
 
-            try {
-                const code = req.query.code
+        try {
+            const code = req.query.code
 
-                if (code) {
-                    const config = {
-                        url: 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/token',
-                        options: {
-                            "grant_type": "authorization_code",
-                            "code": code,
-                            "client_id": process.env.sfmcAuthClientID,
-                            "client_secret": process.env.sfmcAuthClientSecret,
-                            "redirect_uri": "https://twilio-integration-dev.herokuapp.com"
-                        }
+            if (code) {
+                const config = {
+                    url: 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/token',
+                    options: {
+                        "grant_type": "authorization_code",
+                        "code": code,
+                        "client_id": process.env.sfmcAuthClientID,
+                        "client_secret": process.env.sfmcAuthClientSecret,
+                        "redirect_uri": "https://twilio-integration-dev.herokuapp.com"
                     }
-
-                    axios.post(config.url, config.options)
-                        .then((resp) => {
-                            if (!resp.data.access_token) {
-                                res.send('Unauthorized')
-                            } else {
-                                console.log('Access Token Found')
-                                console.log(resp.data.access_token)
-
-                                req.sessionCookies.session.auth = true;
-                                next()
-                            }
-                        })
-                        .catch((err) => {
-                            console.log(err)
-                            res.send(err)
-                        })
-
-                } else {
-                    res.send('Unauthorized: no code provided.')
                 }
-            } catch (err) {
-                console.log(err)
+
+                axios.post(config.url, config.options)
+                    .then((resp) => {
+                        if (!resp.data.access_token) {
+                            res.send('Unauthorized')
+                        } else {
+                            console.log('Access Token Found')
+                            console.log(resp.data.access_token)
+
+                            req.sessionCookies.session.auth = true;
+                            next()
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                        res.send(err)
+                    })
+
+            } else {
+                res.send('Unauthorized: no code provided.')
             }
+        } catch (err) {
+            console.log(err)
         }
-    } else {
-        console.log("auth session: " + req.session.auth)
     }
+    // } else {
+    //     console.log("auth session: " + req.session.auth)
+    // }
 }
