930688062bc7a5e0d1d389dd2a6aceeca095910d
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-08-09 15:58:28 -0500

    session

--- routes/middleware.js
+++ routes/middleware.js
@@ -30,58 +30,59 @@ exports.cookie = function(req, res, next) {
 
 
 exports.authorize = function(req, res, next) {
+    if (req.session.id) {
+        next()
+    }
+
     console.log('*** Authorize Endpoint ***')
         // console.log(req)
-    if (!req.session.id) {
-        if (!req.query.code) {
-            console.log('*** Retrieve Code ***')
 
-            let redirectURI = 'https%3A%2F%2Ftwilio-integration-dev.herokuapp.com';
-            const authBase = 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id='
-            res.redirect(`${authBase}${process.env.sfmcAuthClientID}&redirect_uri=${redirectURI}`)
-        } else if (req.query.code) {
-            console.log('*** Validate Code ***')
-            try {
-                const code = req.query.code
+    if (!req.query.code) {
+        console.log('*** Retrieve Code ***')
 
-                if (code) {
-                    const config = {
-                        url: 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/token',
-                        options: {
-                            "grant_type": "authorization_code",
-                            "code": code,
-                            "client_id": process.env.sfmcAuthClientID,
-                            "client_secret": process.env.sfmcAuthClientSecret,
-                            "redirect_uri": "https://twilio-integration-dev.herokuapp.com"
-                        }
+        let redirectURI = 'https%3A%2F%2Ftwilio-integration-dev.herokuapp.com';
+        const authBase = 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id='
+        res.redirect(`${authBase}${process.env.sfmcAuthClientID}&redirect_uri=${redirectURI}`)
+    } else if (req.query.code) {
+        console.log('*** Validate Code ***')
+        try {
+            const code = req.query.code
+
+            if (code) {
+                const config = {
+                    url: 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/token',
+                    options: {
+                        "grant_type": "authorization_code",
+                        "code": code,
+                        "client_id": process.env.sfmcAuthClientID,
+                        "client_secret": process.env.sfmcAuthClientSecret,
+                        "redirect_uri": "https://twilio-integration-dev.herokuapp.com"
                     }
-
-                    axios.post(config.url, config.options)
-                        .then((resp) => {
-                            if (!resp.data.access_token) {
-                                res.send('Unauthorized')
-                            } else {
-                                console.log('Access Token Found')
-                                console.log(resp.data.access_token)
-
-                                next()
-                            }
-                        })
-                        .catch((err) => {
-                            console.log(err)
-                            res.send(err)
-                        })
-
-                } else {
-                    res.send('Unauthorized: no code provided.')
                 }
-            } catch (err) {
-                console.log(err)
+
+                axios.post(config.url, config.options)
+                    .then((resp) => {
+                        if (!resp.data.access_token) {
+                            res.send('Unauthorized')
+                        } else {
+                            console.log('Access Token Found')
+                            console.log(resp.data.access_token)
+
+                            next()
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                        res.send(err)
+                    })
+
+            } else {
+                res.send('Unauthorized: no code provided.')
             }
+        } catch (err) {
+            console.log(err)
         }
-    } else {
-        console.log(req.session.id)
-        next();
     }
+
 
 }
