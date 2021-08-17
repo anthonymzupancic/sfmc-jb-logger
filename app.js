8f9a4747456555bb4dce9f5ec3af4f19d53fa0db
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-08-17 15:41:40 -0500

    session

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const middleware = require('./routes/middleware');
 //use routes/middleware
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(middleware.authorize);
-app.get('/', express.static(path.join(__dirname, 'views')))
+app.use(express.static(path.join(__dirname, 'views')))
 
 
 // Express in Development Mode
