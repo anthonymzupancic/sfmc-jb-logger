eea52f23f9db4afea62c8689bc1deefdff72843c
Tony Zupancic <74277756+WinfieldCreative@users.noreply.github.com>
2021-08-20 16:59:40 -0500

    fix execute

--- routes/middleware.js
+++ routes/middleware.js
@@ -21,7 +21,7 @@ exports.authorize = function(req, res, next) {
         if (!req.query.code) {
             console.log('*** Retrieve Code ***')
 
-            let redirectURI = 'https%3A%2F%2Ftwilio-integration-dev.herokuapp.com/views';
+            let redirectURI = 'https%3A%2F%2Ftwilio-integration-dev.herokuapp.com/';
             const authBase = 'https://mc1q10jrzwsds3bcgk0jjz2s8h80.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id='
             res.redirect(`${authBase}${process.env.sfmcAuthClientID}&redirect_uri=${redirectURI}`)
         } else if (req.query.code) {
@@ -37,7 +37,7 @@ exports.authorize = function(req, res, next) {
                             "code": code,
                             "client_id": process.env.sfmcAuthClientID,
                             "client_secret": process.env.sfmcAuthClientSecret,
-                            "redirect_uri": "https://twilio-integration-dev.herokuapp.com"
+                            "redirect_uri": "https://twilio-integration-dev.herokuapp.com/views"
                         }
                     }
 
